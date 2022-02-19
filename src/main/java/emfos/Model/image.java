409053b96085230780c58c3dd4680d5522129be4
Fatihah Yunus <nurfatihahyunus7@gmail.com>
2022-02-19 23:09:19 +0800

    try deployed

--- src/main/java/emfos/Model/image.java
+++ src/main/java/emfos/Model/image.java
@@ -1,6 +1,27 @@
 package emfos.Model;
 
 public class image {
+    private int imageid;
+    private String imagename;
 
+    public image(int imageid, String imagename) {
+        this.imageid = imageid;
+        this.imagename = imagename;
+    }
 
+    public int getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(int imageid) {
+        this.imageid = imageid;
+    }
+
+    public String getImagename() {
+        return imagename;
+    }
+
+    public void setImagename(String imagename) {
+        this.imagename = imagename;
+    }
 }
